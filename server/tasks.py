03e852420e0cb487878bdacc5747d9bbee36966a
Daniel Mendoza <daltemen@hotmail.com>
2016-11-07 01:24:47 -0500

    Added get data with Redis

--- server/tasks.py
+++ server/tasks.py
@@ -1,5 +1,6 @@
 import os
 import time
+import redis
 from datetime import datetime, timedelta
 import pymongo
 from bson.json_util import dumps, loads
@@ -7,7 +8,12 @@ from bson.json_util import dumps, loads
 from fuzzy_logic import fuzzy_logic_hearth
 
 from celery import Celery
-DATABASE_HEARTH="MONGO"
+
+
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+SET_REDIS = "pulsos"
+
+DATABASE_HEARTH = "MONGO"
 
 celery = Celery("tasks", broker="amqp://guest:guest@localhost//")
 celery.conf.result_backend = os.environ.get('result_backend', 'amqp')
@@ -28,14 +34,28 @@ celery.conf.timezone = 'UTC'
 
 @celery.task
 def guardar_Data(mensaje, llegada):
-    if "var" in mensaje:
-        insert = database.pulsos.insert({
-                                    "valor": mensaje["var"],    				    
-                                    "llegada": datetime.strptime(llegada, "%Y-%m-%dT%H:%M:%S")
-                                 })
-        print (mensaje["cont"])
+    if DATABASE_HEARTH == "REDIS":
+    	#r.zadd('prueba',r.time()[0],{"valor":mensaje["var"],"llegada":datetime.strptime(llegada, "%Y-%m-%dT%H:%M:%S")})
+    	r.zadd('prueba',r.time()[0],{"valor":mensaje["var"],"llegada":datetime.strptime(llegada, "%Y-%m-%dT%H:%M:%S")})
+	    if "var" in mensaje:
+		    '''insert = database.pulsos.insert({
+		                                "valor": mensaje["var"],    				    
+		                                "llegada": datetime.strptime(llegada, "%Y-%m-%dT%H:%M:%S")
+		                             })'''
+		    print (mensaje["cont"])
+		else:
+		    print ("invalido") 
+		
     else:
-        print ("invalido")
+    	if "var" in mensaje:
+		    insert = database.pulsos.insert({
+		                                "valor": mensaje["var"],    				    
+		                                "llegada": datetime.strptime(llegada, "%Y-%m-%dT%H:%M:%S")
+		                             })
+		    print (mensaje["cont"])
+		else:
+		    print ("invalido")
+
 
 @celery.task
 def obtener_pulso_mongo():
@@ -62,7 +82,9 @@ def obtener_pulso_mongo():
 
 @celery.task
 def obtener_pulso_redis():
-	print "ME LLAMARON CADA 60 SEGUNDOS"
+	#example query last_day 
+	query =	r.zrangebyscore('prueba', r.time()[0]-86400000, r.time()[0])
+
 
 if __name__ == "__main__":
     celery.start()
