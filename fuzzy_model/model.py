50291c78b447d4e818bf61252d02d3d842a89ff5
Daniel Mendoza <daltemen@hotmail.com>
2017-02-13 01:20:54 -0500

    Added first model's version fuzzy-logic

---
+++ fuzzy_model/model.py
@@ -0,0 +1,39 @@
+import numpy as np
+import time
+import skfuzzy as fuzz
+from skfuzzy import control as ctrl
+
+# New Antecedent/Consequent objects hold universe variables and membership
+# functions
+hearthr = ctrl.Antecedent(np.arange(0, 201, 1), 'hearthr')
+#import pdb;pdb.set_trace()
+alarm = ctrl.Consequent(np.arange(0, 101, 1), 'alarm')
+
+hearthr['brady'] = fuzz.trimf(hearthr.universe, [0, 0, 60])
+hearthr['normo'] = fuzz.trapmf(hearthr.universe, [30,60,100,130])
+hearthr['tachy'] = fuzz.trapmf(hearthr.universe, [100, 120, 400,1000])
+
+alarm['alarm_brady'] = fuzz.trimf(alarm.universe, [0, 0, 50])
+alarm['no_alarm'] = fuzz.trimf(alarm.universe, [0, 50, 100])
+alarm['alarm_tachy'] = fuzz.trimf(alarm.universe, [50, 100, 101])
+'''hearthr.view()
+alarm.view()
+raw_input("Press Enter to continue...")'''
+
+rule1 = ctrl.Rule(hearthr['brady'], alarm['alarm_brady'])
+rule2 = ctrl.Rule(hearthr['normo'], alarm['no_alarm'])
+rule3 = ctrl.Rule(hearthr['tachy'], alarm['alarm_tachy'])
+
+#rule1.view()
+#raw_input("Press Enter to continue...")
+
+
+tipping_ctrl = ctrl.ControlSystem([rule1, rule2, rule3])
+tipping = ctrl.ControlSystemSimulation(tipping_ctrl)
+tipping.input['hearthr'] = 200
+tipping.compute()
+print tipping.output['alarm']
+alarm.view(sim=tipping)
+input("Press Enter to continue...")
+
+
